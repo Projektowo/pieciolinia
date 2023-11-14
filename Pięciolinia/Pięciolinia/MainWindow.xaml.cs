4b88152fe8f6b05203c88a2cf24a22376fb4043c
sanczan <97993763+sanczan@users.noreply.github.com>
2023-11-14 19:54:26 +0100

    AdaptDragAndDropForAllRectangles

--- Pięciolinia/Pięciolinia/MainWindow.xaml.cs
+++ Pięciolinia/Pięciolinia/MainWindow.xaml.cs
@@ -21,26 +21,86 @@ namespace Pięciolinia
     public partial class MainWindow : Window
     {
 
-        private void RectangleMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragDrop.DoDragDrop(Red, Red, DragDropEffects.Move);
-            }
-        }
+        //private void RectangleMouseMove1(object sender, MouseEventArgs e)
+        //{
+        //    if (e.LeftButton == MouseButtonState.Pressed)
+        //    {
+        //        DragDrop.DoDragDrop(Red1, Red1, DragDropEffects.Move);
+        //    }
+        //}
+
 
         private void CanvasDrop(object sender, DragEventArgs e)
         {
 
         }
 
+        //private void CanvasDragOver(object sender, DragEventArgs e)
+        //{
+        //    Point dropPositione = e.GetPosition(Canvas);
+
+        //    Canvas.SetLeft(Red, dropPositione.X - 25);
+        //    Canvas.SetTop(Red, dropPositione.Y - 25);
+        //}
+
         private void CanvasDragOver(object sender, DragEventArgs e)
         {
-            Point dropPositione = e.GetPosition(Canvas);
+            if (sender is Rectangle draggedRectangle)
+            {
+                Point dropPosition = e.GetPosition(MainCanvas);
 
-            Canvas.SetLeft(Red, dropPositione.X - 25);
-            Canvas.SetTop(Red, dropPositione.Y - 25);
+                Canvas.SetLeft(draggedRectangle, dropPosition.X - (draggedRectangle.Width / 2));
+                Canvas.SetTop(draggedRectangle, dropPosition.Y - (draggedRectangle.Height / 2));
+            }
         }
+
+        private bool isDragging = false;
+        private Point offset;
+
+        private void Canvas_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                if (e.Source is Rectangle draggedRectangle)
+                {
+                    draggedRectangle.DragEnter += Rectangle_DragEnter;
+                    draggedRectangle.Stroke = Brushes.Blue;
+                    e.Effects = DragDropEffects.Copy;
+                }
+            }
+        }
+
+        private void Rectangle_DragEnter(object sender, DragEventArgs e)
+        {
+            // Your DragEnter logic for individual rectangles
+        }
+
+        private void Rectangle_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            offset = e.GetPosition((UIElement)sender);
+            ((UIElement)sender).CaptureMouse();
+        }
+
+        private void Rectangle_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point currentPosition = e.GetPosition(MainCanvas);
+                double newX = currentPosition.X - offset.X;
+                double newY = currentPosition.Y - offset.Y;
+
+                Canvas.SetLeft((UIElement)sender, newX);
+                Canvas.SetTop((UIElement)sender, newY);
+            }
+        }
+
+        private void Rectangle_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            ((UIElement)sender).ReleaseMouseCapture();
+        }
+
 
         public MainWindow()
         {
