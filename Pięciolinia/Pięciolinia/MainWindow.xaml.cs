37ff8d0469b48dc7d37ae1ead5a0074a40ab8ef8
Kamil Ptak <kamil2001p@gmail.com>
2023-11-16 18:17:51 +0100

    Add tact validation

--- Pięciolinia/Pięciolinia/MainWindow.xaml.cs
+++ Pięciolinia/Pięciolinia/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -106,5 +107,12 @@ namespace Pięciolinia
         {
             InitializeComponent();
         }
+
+        private void TactValidation(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9/]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
     }
 }
