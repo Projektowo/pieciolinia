6f29ab8dd0e2caf80dad75d04ae936d88d6fe758
sanczan <97993763+sanczan@users.noreply.github.com>
2023-12-31 19:31:34 +0100

    AddComments

--- Pięciolinia/Pięciolinia/MainWindow.xaml.cs
+++ Pięciolinia/Pięciolinia/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace Pięciolinia
     public partial class MainWindow : Window
     {
 
+        //elementy które są nutami
         private UIElement selectedElement;
         private UIElement[] elements;
 
@@ -28,23 +29,26 @@ namespace Pięciolinia
             InitializeElements();
             SelectElement(elements[0]);
             SelectElement(elementImage1);
-            this.MinWidth = 920;
-            this.MinHeight = 600;
 
         }
 
+
+        //walidacja textboxa na takt
         private void TactValidation(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9/]+");
             e.Handled = regex.IsMatch(e.Text);
         }
 
+        //inicjalizowanie elementów/nut dodając je do tablicy
         private void InitializeElements()
         {
             // Add all your Image controls to the array
             elements = new UIElement[] { elementImage1, elementImage2 };
         }
 
+
+        //wybieranie nuty za pomocą klawiszy lewo/prawo
         private void SelectElement(UIElement element)
         {
             // Deselect the currently selected element
@@ -58,49 +62,54 @@ namespace Pięciolinia
             // Highlight the selected element, adjust any visual changes as needed
         }
 
+
+        //przycisk do dodawania taktów
         private void AddColumnButton_Click(object sender, RoutedEventArgs e)
         {
             AddColumnToGrid();
         }
 
+
+        //póki co jest tylko jedna kolumna zamiast całego taktu
         private void AddColumnToGrid()
         {
             ColumnDefinition columnDefinition = new ColumnDefinition();
             mainGrid.ColumnDefinitions.Add(columnDefinition);
 
-            // Add content to the new column if needed
-            // For example, add a button in the new column
+            //przycisk do wizualizacji czegokolwiek
             Button newButton = new Button();
             newButton.Content = "New Button";
-            Grid.SetColumn(newButton, mainGrid.ColumnDefinitions.Count - 1); // Set the button to the last column
+            Grid.SetColumn(newButton, mainGrid.ColumnDefinitions.Count - 1);
             mainGrid.Children.Add(newButton);
         }
 
+
+        //logika strzałek/klawiszy
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-            double step = 5; // Set the step size for movement
-
             switch (e.Key)
             {
                 case Key.Left:
-                    // Move the selection to the previous element
+                    // zmiana nuty na nutę po lewej
                     int currentIndex = Array.IndexOf(elements, selectedElement);
                     int newIndex = (currentIndex - 1 + elements.Length) % elements.Length;
                     SelectElement(elements[newIndex]);
                     break;
 
                 case Key.Right:
-                    // Move the selection to the next element
+                    // zmiana nuty na nutę po prawej
                     currentIndex = Array.IndexOf(elements, selectedElement);
                     newIndex = (currentIndex + 1) % elements.Length;
                     SelectElement(elements[newIndex]);
                     break;
 
                 case Key.Up:
+                    //przesunięcie nuty wyżej
                     Grid.SetRow(selectedElement, Grid.GetRow(selectedElement) - 1);
                     break;
 
                 case Key.Down:
+                    //przesunięcie nuty niżej
                     Grid.SetRow(selectedElement, Grid.GetRow(selectedElement) + 1);
                     break;
             }
